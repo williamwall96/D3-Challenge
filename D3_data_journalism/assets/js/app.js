f49b042f6b071831d1c78d6e88fc79efd9a9d0f4
Will Walles <willwalles@Wills-MacBook-Pro.local>
2021-04-09 19:45:45 -0500

    Added more code for the scatter plot

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -23,3 +23,31 @@ var svg = d3
 // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(stateData) {
+  // print the data to see what's up
+  statesData = stateData;
+  console.log(statesData);
+
+  // Parse Data/Cast as numbers
+  statesData.forEach(function(data) {
+    data.healthcare = +data.healthcare;
+    data.poverty = +data.poverty;
+    var abbrevs = data.abbr;
+  });
+
+  // Create Scale Functions
+  var xLinearScale = d3.scaleLinear()
+      .domain(d3.extent(statesData, d => d.poverty))
+      .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+      .domain(d3.extent(statesData, d => d.healthcare))
+      .range([height, 0]);
+
+  // Create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+  
+});
