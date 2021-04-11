cda100e9b9b408e7aab695de0f09709c7ad60c15
Will Walles <willwalles@Wills-MacBook-Pro.local>
2021-04-11 14:56:06 -0500

    Scatter plot is looking nice from app.js, now beginning to work on html formatting

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -39,15 +39,71 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
 
   // Create Scale Functions
   var xLinearScale = d3.scaleLinear()
-      .domain(d3.extent(statesData, d => d.poverty))
+      .domain([8, 23])
       .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-      .domain(d3.extent(statesData, d => d.healthcare))
+      .domain([3, 27])
       .range([height, 0]);
 
   // Create axis functions
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
   
+  // Append Axes to the chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // Create circles for scatter plot
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(statesData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "9")
+    .attr("fill", "rgb(117, 145, 197)")
+    .attr("opacity", ".9");
+
+  // create circleLabels variable
+  var circleLabels = chartGroup.selectAll(null).data(statesData).enter().append('text');
+
+  // add data for cirlce labels
+  circleLabels
+  .attr("x", function(d) {
+    return xLinearScale(d.poverty);
+  })
+  .attr("y", function(d) {
+    return yLinearScale(d.healthcare);
+  })
+  .text(function(d) {
+    return d.abbr;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "8px")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white");``
+  
+
+  // Create axes labels
+  
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
+}).catch(function(error) {
+console.log(error);
 });
+  
